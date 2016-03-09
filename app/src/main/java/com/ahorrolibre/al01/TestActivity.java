19380e53799171dbcaa850d79085a558cb56ec90
rjgamez <rafa_tj@hotmail.com>
2016-03-08 19:14:09 -0800

    Adding 2 second splash screen on launch (with option to tap screen to continue), Splashscreen also DOES NOT go on the backstack

--- app/src/main/java/com/ahorrolibre/al01/TestActivity.java
+++ app/src/main/java/com/ahorrolibre/al01/TestActivity.java
@@ -2,34 +2,73 @@ package com.ahorrolibre.al01;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import java.util.concurrent.TimeUnit;
+import android.view.MotionEvent;
 
 public class TestActivity extends AppCompatActivity
 {
 
-    @Override
+  /*  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
 
-        Intent cambio = new Intent(this, MainActivity.class);
-        startActivity(cambio);
 
 
+
+    }*/
+
+
+    private Thread thread;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+      //  final MyActivity myActivity = this;
+
+        thread=  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        wait(2000);
+                    }
+                }
+                catch(InterruptedException ex){
+                }
+
+                continueToMain();
+            }
+        };
+
+        thread.start();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent evt)
+    {
+        if(evt.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            synchronized(thread){
+                thread.notifyAll();
+            }
+        }
+        continueToMain();
+        return true;
+    }
 
-   }
+    public void continueToMain (){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+
+        /*Intent cambio = new Intent(this, MainActivity.class);
+        startActivity(cambio);*/
+    }
+
+}
